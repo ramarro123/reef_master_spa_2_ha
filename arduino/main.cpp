b7876e6f92e96fe249d5a62a347671c6a5dcf474
rama <rama@Mac-mini-di-rama.local>
2025-06-18 08:25:53 +0200

    import

---
+++ arduino/main.cpp
@@ -0,0 +1,245 @@
+#include "esp_camera.h"
+#include "ESP32_OV5640_AF.h"
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+//            You must select partition scheme from the board menu that has at least 3MB APP space.
+//            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15
+//            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
+
+// ===================
+// Select camera model
+// ===================
+// #define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+// #define CAMERA_MODEL_ESP_EYE  // Has PSRAM
+// #define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
+// #define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+// #define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
+// #define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
+#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
+// #define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
+// #define CAMERA_MODEL_M5STACK_CAMS3_UNIT  // Has PSRAM
+// #define CAMERA_MODEL_AI_THINKER // Has PSRAM
+// #define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+// #define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
+//  ** Espressif Internal Boards **
+// #define CAMERA_MODEL_ESP32_CAM_BOARD
+// #define CAMERA_MODEL_ESP32S2_CAM_BOARD
+// #define CAMERA_MODEL_ESP32S3_CAM_LCD
+// #define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
+// #define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
+#include "camera_pins.h"
+
+void startCameraServer();
+void setupLedFlash(int pin);
+OV5640 ov5640 = OV5640();
+WiFiMulti wifiMulti;
+const uint32_t connectTimeoutMs = 10000;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  WiFi.mode(WIFI_STA);
+
+  wifiMulti.addAP("WIFI", "PASSWORD");
+  wifiMulti.addAP("WIFI2", "PASSWORD2");
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sccb_sda = SIOD_GPIO_NUM;
+  config.pin_sccb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 13000000;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  // config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 10;
+  config.fb_count = 1;
+
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG)
+  {
+    if (psramFound())
+    {
+      Serial.printf("psramFound!\n");
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    }
+    else
+    {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
+  }
+  else
+  {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+#if CONFIG_IDF_TARGET_ESP32S3
+    config.fb_count = 2;
+#endif
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  Serial.printf("frame_size %d\n", config.frame_size);
+  Serial.printf("jpeg_quality %d\n", config.jpeg_quality);
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK)
+  {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t *s = esp_camera_sensor_get();
+  ov5640.start(s);
+  if (ov5640.focusInit() == 0)
+  {
+    Serial.println("OV5640_Focus_Init Successful!");
+  }
+
+  if (ov5640.autoFocusMode() == 0)
+  {
+    Serial.println("OV5640_Auto_Focus Successful!");
+  }
+
+  // initial sensors are flipped vertically and colors are a bit saturated
+  if (s->id.PID == OV3660_PID)
+  {
+    s->set_vflip(s, 1);       // flip it back
+    s->set_brightness(s, 1);  // up the brightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
+  }
+  // drop down frame size for higher initial frame rate
+  if (config.pixel_format == PIXFORMAT_JPEG)
+  {
+    s->set_framesize(s, FRAMESIZE_QVGA);
+  }
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+#if defined(CAMERA_MODEL_ESP32S3_EYE)
+  s->set_vflip(s, 1);
+#endif
+
+// Setup LED FLash if LED pin is defined in camera_pins.h
+#if defined(LED_GPIO_NUM)
+  setupLedFlash(LED_GPIO_NUM);
+#endif
+
+  if (wifiMulti.run() == WL_CONNECTED)
+  {
+  }
+
+  startCameraServer();
+
+  // RAMA EXTRA CONFIG
+  Serial.println("Apply rama preference");
+  s->set_quality(s, 10);
+  s->set_brightness(s, -3);
+  s->set_contrast(s, 3);
+  s->set_saturation(s, 4);
+  s->set_sharpness(s, 3);
+  s->set_denoise(s, 8);
+  s->set_ae_level(s, 5);
+  s->set_gainceiling(s, (gainceiling_t)114);
+  s->set_special_effect(s, 1);
+
+  s->set_hmirror(s, 1);
+  s->set_vflip(s, 0);
+  s->set_framesize(s, FRAMESIZE_SXGA);
+  s->set_xclk(s, LEDC_TIMER_0, 13);
+
+
+}
+
+unsigned long get_fw_status_next = 0;
+unsigned long wifimulti_next = 0;
+
+void loop()
+{
+  unsigned long _mil = millis();
+
+  if ((unsigned long)(_mil - get_fw_status_next) > 2000)
+  {
+    get_fw_status_next = _mil;
+    uint8_t rc = ov5640.getFWStatus();
+    Serial.printf("FW_STATUS = 0x%x\n", rc);
+    if (rc == -1)
+    {
+      Serial.println("Check your OV5640");
+    }
+    else if (rc == FW_STATUS_S_FOCUSED)
+    {
+      Serial.println("Focused!");
+    }
+    else if (rc == FW_STATUS_S_FOCUSING)
+    {
+      Serial.println("Focusing!");
+    }
+    else
+    {
+    }
+  }
+  
+  uint8_t wmr = wifiMulti.run();
+  if ((unsigned long)(_mil - wifimulti_next) > 5000)
+  {
+    wifimulti_next = _mil;
+    if (wifiMulti.run() == WL_CONNECTED)
+    {
+      Serial.print("WiFi connected: ");
+      Serial.print(WiFi.SSID());
+      Serial.print(" ");
+      Serial.println(WiFi.RSSI());
+
+      Serial.print("Camera Ready! Use 'http://");
+      Serial.print(WiFi.localIP());
+      Serial.println("' to connect");
+    }
+    else
+    {
+      Serial.println("WiFi not connected!");
+    }
+  }
+
+
+  // Do nothing. Everything is done in another task by the web server
+  delay(2);
+}
